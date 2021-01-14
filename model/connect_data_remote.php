668d1019fa57d193f6922752d29b563d561b896c
Galder <galdergomez50@gmail.com>
2021-01-14 08:58:53 +0100

    connect data remote

---
+++ model/connect_data_remote.php
@@ -0,0 +1,8 @@
+<?php
+class connect_data{
+    public $host="localhost";
+    public $userbbdd="grupo4ze";
+    public $passbbdd="grupo4GMX";
+    public $ddbbname="grupo4ze_reto4";
+    
+}
--- model/productoTiendaModel.php
+++ model/productoTiendaModel.php
@@ -1,6 +1,10 @@
 <?php
+if($_SERVER["SERVER_NAME"]=="grupo4.zerbitzaria.net"){
+    include_once("connect_data_remote.php");
+}else{
+    include_once("connect_data.php");
+}
 
-include_once 'connect_data.php';
 include_once 'productoTiendaClass.php';
 include_once 'tiendaModel.php';
 
