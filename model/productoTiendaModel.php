5bc48c619811dc4cda18a18af406e74a3df75a75
Galder <galdergomez50@gmail.com>
2021-01-14 09:05:04 +0100

    include connect data remote

--- model/productoTiendaModel.php
+++ model/productoTiendaModel.php
@@ -4,7 +4,6 @@ if($_SERVER["SERVER_NAME"]=="grupo4.zerbitzaria.net"){
 }else{
     include_once("connect_data.php");
 }
-
 include_once 'productoTiendaClass.php';
 include_once 'tiendaModel.php';
 
--- model/ventaModel.php
+++ model/ventaModel.php
@@ -1,6 +1,9 @@
 <?php
-
-include_once 'connect_data.php';
+if($_SERVER["SERVER_NAME"]=="grupo4.zerbitzaria.net"){
+    include_once("connect_data_remote.php");
+}else{
+    include_once("connect_data.php");
+}
 include_once 'ventaClass.php';
 
 class ventaModel extends ventaClass{
