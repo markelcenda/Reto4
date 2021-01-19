8f4cec292aea98e1a762ab1cd622a1cd79880baf
markelcenda <markelcenda@gmail.com>
2021-01-19 10:20:28 +0100

    prueba comentarios V2

--- controller/cComentario.php
+++ controller/cComentario.php
@@ -2,16 +2,14 @@
 
 $data=json_decode(file_get_contents("php://input"),true);
 
-$email=$data['email'];
-$asunto=$data['asunto'];
-$mensaje=$data['mensaje'];
+
 
 ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
-    //$from = "anonimo";
+    $email=$data['email'];
+    $asunto=$data['asunto'];
+    $mensaje=$data['mensaje'];
     $to = "mmanemane932@gmail.com";
-    //$subject = "Checking PHP mail";
-    //$message = "PHP mail works just fine";
     $headers = "De:" . $email;
     if(mail($to,$asunto,$mensaje, $headers)){
         echo "El email fue enviado"; 
