b40550e4737c6f401bfafc57901d7283108d6a6e
Xabi <xaguirrearrien@gmail.com>
2021-01-21 12:06:04 +0100

    scrollFunction repuesto

--- view/js/jsFunctions.js
+++ view/js/jsFunctions.js
@@ -24,6 +24,22 @@ function leerMas() {
 
 }
 
+var mybutton = document.getElementById("topBtn");
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 var miApp = angular.module("miApp", []);
 miApp.controller("miControlador", function ($scope, $http) {
 
