7ad1dcd15af61d8134270ee7ac90dea80e591dc4
Xabi <xaguirrearrien@gmail.com>
2021-01-21 12:33:41 +0100

    redes del footer mejorado

--- view/js/jsFunctions.js
+++ view/js/jsFunctions.js
@@ -24,9 +24,11 @@ function leerMas() {
 
 }
 
-var mybutton = document.getElementById("topBtn");
+ varmybutton = document.querySelector("#topBtn");
 
 function scrollFunction() {
+
+    
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.display = "block";
     } else {
