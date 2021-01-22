dce2e9f5f102b7c7abb94f8480116677ff607860
Xabi <xaguirrearrien@gmail.com>
2021-01-22 20:00:37 +0100

    cambio color de fondo index y orden de las cosas

--- view/js/jsFunctions.js
+++ view/js/jsFunctions.js
@@ -64,8 +64,11 @@ miApp.controller("miControlador", function ($scope, $http) {
 
         $http.get(url)
             .then(function (response) {
+
+                console.log(response.data.listUltimasTres);
                 
                 $scope.tiendas = response.data.listUltimasTres;
+                
 
             });
 
