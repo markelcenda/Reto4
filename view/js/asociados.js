6a1074ad7e53b4ca07ebf567c5dd551f1a5b7245
Xabi <xaguirrearrien@gmail.com>
2021-01-25 10:18:03 +0100

    tipos no repetidos

--- view/js/asociados.js
+++ view/js/asociados.js
@@ -6,7 +6,33 @@ myApp.controller('myController', ['$scope', '$http', function ($scope, $http) {
 
     $scope.load = () => {
         $http.get('../../controller/cTiendas.php').then(function (response) {
+
             $scope.listaAsociados = response.data.list;
+
+            var lista = response.data.list;
+
+            $scope.listaTipos = [];
+
+            $scope.listaTipos.push({ tipo: lista[0].tipo });
+
+            for (let i = 0; i < lista.length; i++) {
+
+                var nuevoTipo = true;
+
+                for (let j = 0; j < $scope.listaTipos.length; j++) {
+
+                    if (lista[i].tipo == $scope.listaTipos[j].tipo) {
+                        nuevoTipo = false;
+                        break;
+                    }
+
+                }
+
+                if (nuevoTipo) {
+                    $scope.listaTipos.push({ tipo: lista[i].tipo });
+                }
+
+            }
         })
     }
 
