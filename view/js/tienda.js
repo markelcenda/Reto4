4712d0834fd3716a77d5eb8174b6689baf99e124
markelcenda <markelcenda@gmail.com>
2021-01-26 09:15:19 +0100

    añadir modal de registro de usuarios en todas las paginas

--- view/js/tienda.js
+++ view/js/tienda.js
@@ -172,4 +172,54 @@ myApp.controller('myController', ['$scope', '$http', function ($scope, $http) {
             }
         }
     }
+
+    $scope.error = "";
+    $scope.username = "";
+    $scope.nombre = "";
+    $scope.apellidos = "";
+    $scope.email = "";
+    $scope.password = "";
+
+    //Funcion de registro de usuario
+    $scope.registrarse = function () {
+
+        if ($scope.username == "" || $scope.nombre == "" || $scope.apellidos == "" || $scope.email == "" || $scope.password == "") {
+            $scope.error = "Ninguno de los campos puede estar vacio";
+        } else {
+
+            //Detecta si el formato del correo electronico es valio para permitir el registro
+            if (/\w+\@\w+\.\w+/.test($scope.email)) {
+
+                var url = "../../controller/cInsertarUsuario.php";
+                var data = { 'username': $scope.username, 'nombre': $scope.nombre, 'apellidos': $scope.apellidos, 'email': $scope.email, 'password': $scope.password };
+
+                $http.post(url, data)
+                    .then(function (response) {
+
+                        if (response.data.error == "Registro realizado con exito") {
+
+                            alert(response.data.error);
+                            window.location.reload();
+
+                        } else {
+                            $scope.error = response.data.error;
+                        }
+                    });
+            } else {
+                $scope.error = "El correo electronico no es valido";
+            }
+        }
+    }
+
+    //Vacia todas las variables al cerrar el modal de registro
+    $scope.cerrarRegistro = function () {
+        $scope.error = "";
+        $scope.username = "";
+        $scope.nombre = "";
+        $scope.apellidos = "";
+        $scope.email = "";
+        $scope.password = "";
+    }
+
+
 }]);
